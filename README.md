ccb0b4cea36ccc740d62e25b42ad816a7fba8df3
Giuseppe Pagliuca <giuseppe.pagliuca@gmail.com>
2025-07-29 22:49:57 +0200

    first working version

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Laida
+
+Based on data from [glances](https://github.com/nicolargo/glances?tab=readme-ov-file) this package provides a [Streamlit](https://streamlit.io/) UI for small displays.
+
+The idea is to provide linux users something similar to Aida64 dashboards (long term goal :-)).
--- laida/app.py
+++ laida/app.py
@@ -1,48 +1,99 @@
+__author__ = "Giuseppe Pagliuca"
+__version__ = "0.0.1"
+
 import time
 
 import matplotlib.pyplot as plt
 import requests
 import streamlit as st
+import yaml
+
+plt.style.use("fivethirtyeight")
+
+
+## Functions
+# ------------------------------------------------------------------------------
+def get_system():
+    r = requests.get("http://{}:61208/api/4/system/".format(server))
+    return r.json()
+
+
+def get_gpu():
+    r = requests.get("http://{}:61208/api/4/gpu/".format(server))
+    return r.json()
+
+
+def get_cpu():
+    r = requests.get("http://{}:61208/api/4/cpu".format(server))
+    return r.json()
+
+
+def get_mem():
+    r = requests.get("http://{}:61208/api/4/mem".format(server))
+    return r.json()
+
+
+def get_sensors():
+    r = requests.get("http://{}:61208/api/4/sensors".format(server))
+    return r.json()
+
+
+def get_ip():
+    r = requests.get("http://{}:61208/api/4/ip".format(server))
+    return r.json()
+
+
+## Settings
+# ------------------------------------------------------------------------------
+with open("../data/settings.yaml", "r") as fobj:
+    settings = yaml.safe_load(fobj)
+    server = settings["ip"]
 
 if "counter" not in st.session_state:
     st.session_state.counter = 0
 
 st.session_state.counter += 1
 
-
-def get_system():
-    r = requests.get("http://192.168.178.47:61208/api/4/system/")
-    return r.json()
-
-
-def get_gpu():
-    r = requests.get("http://192.168.178.47:61208/api/4/gpu/")
-    return r.json()
-
-
-def get_cpu():
-    r = requests.get("http://192.168.178.47:61208/api/4/cpu")
-    return r.json()
-
-
-def get_mem():
-    r = requests.get("http://192.168.178.47:61208/api/4/mem")
-    return r.json()
-
-
+## Main
+# ------------------------------------------------------------------------------
 system = get_system()
 cpu = get_cpu()
 gpu = get_gpu()
 mem = get_mem()
+ip = get_ip()
+sensors = get_sensors()
 
+## UI
+# ------------------------------------------------------------------------------
 
+# System info
+st.table(system)
+
+# Metrics
+st.header("GPU metrics", divider=True)
+c0, c1, c2 = st.columns(3)
+with c0:
+    st.metric("Temperature", str(gpu[1]["temperature"]) + " °C")
+with c1:
+    st.metric("Fan speed", gpu[1]["fan_speed"])
+with c2:
+    st.metric("Memory", gpu[1]["mem"])
+
+# Donut plots
 c0, c1, c2 = st.columns(3)
 with c0:
     st.header("CPU")
     width = 0.3
     vals = [cpu["total"], 60]
     fig, ax = plt.subplots()
-    ax.pie(vals, startangle=90, wedgeprops={"width": width})
+    ax.pie(
+        vals,
+        startangle=90,
+        colors=["r", "silver"],
+        wedgeprops={
+            "width": width,
+        },
+    )
     st.pyplot(fig)
 
 with c1:
@@ -50,7 +101,7 @@ with c1:
     width = 0.3
     vals = [gpu[1]["proc"], 60]
     fig, ax = plt.subplots()
-    ax.pie(vals, startangle=90, wedgeprops={"width": width})
+    ax.pie(vals, startangle=90, colors=["r", "silver"], wedgeprops={"width": width})
     st.pyplot(fig)
 
 with c2:
@@ -58,10 +109,9 @@ with c2:
     width = 0.3
     vals = [mem["percent"], 60]
     fig, ax = plt.subplots()
-    ax.pie(vals, startangle=90, wedgeprops={"width": width})
+    ax.pie(vals, startangle=90, colors=["r", "silver"], wedgeprops={"width": width})
     st.pyplot(fig)
 
 while True:
     time.sleep(5)
-    st.header(f"This page has run {st.session_state.counter} times.")
     st.rerun()
